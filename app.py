e127a35265d1578d244b6c16fbaf560361bfb4ba
yesseyev <yesseyev.mt@gmail.com>
2019-05-14 16:34:32 +0300

    dynamic images with python and script to change image were implemented

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 import re
 import sys
 import os
+import random
 # sys.path.append(os.path.abspath('./model'))
 
 # from load import *
@@ -16,10 +17,24 @@ app = Flask(__name__)
 global model, graph
 # model, graph = init()
 
+global img_path, img_src_template
+img_path, img_src_template = './static/img/cells', '.%s/%s'
+
+
+@app.route('/change-image', methods=['POST'])
+def change():
+    img_files = os.listdir(img_path)
+    random_filename = img_files[int(random.random() * len(img_files))]
+
+    return img_src_template % (img_path, random_filename)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    img_files = os.listdir(img_path)
+    random.shuffle(img_files)
+    images = [img_src_template % (img_path, filename) for filename in img_files[:4]]
+    return render_template('index.html', images=images)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,31 @@
 function allowDrop(ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  var copy = $('#' + data).clone();
-  var target = $(ev.target);
-  var placeholder = target.hasClass('dropBox') ? target : target.parent('.dropBox');
-  console.log(placeholder);
-  placeholder.empty().append(copy);
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var copy = $('#' + data).clone().removeAttr('id');
+    var target = $(ev.target);
+    var placeholder = target.hasClass('dropBox') ? target : target.parent('.dropBox');
+    console.log(placeholder);
+    placeholder.empty().append(copy);
+}
+
+function changeImage(target) {
+    console.log('image-change');
+    var parent = $(target).parents('div.card');
+    // console.log(parent, parent.parents('div.card'));
+    var targetImg = $(parent).find('img');
+    console.log(targetImg);
+
+    $.post('/change-image', function (img_src) {
+        console.log(img_src);
+
+        targetImg.attr('src', img_src);
+    });
 }
